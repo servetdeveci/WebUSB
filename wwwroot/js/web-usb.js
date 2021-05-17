258b084ca717b04ebccf387c7697e69428fbc3ac
Servet DEVECİ <servetdeveci@atlassayac.com>
2021-05-17 15:53:58 +0300

    genel konfigurasyona göre veri alınması yada gönderilmesi yapıldı

--- wwwroot/js/web-usb.js
+++ wwwroot/js/web-usb.js
@@ -1,12 +1,53 @@
-﻿$(document).ready(function () {    
-
+﻿$(document).ready(function () {
+    listele();
 });
 
 var counter = 0;
+var currDevices;
 
-async function oku() {
-    navigator.usb.requestDevice({ filters: [] }).then(devices => {});
+function izinIste() {
+    navigator.usb.requestDevice({ filters: [] }).then(devices => { currDevices = devices });
+    console.log(currDevices);
+
+    // eğer ürün bilgileri biliniyorsa spesifik olarak izin istenebilir.
+    // örnek: 
+    //filters: [{
+    //    vendorId: 0xABCD,
+    //    classCode: 0xFF, // vendor-specific
+    //    protocolCode: 0x01
+    //}]
+
 }
+
+async function SecileneIzinIste(_vendorId) {
+    $("#secilen").html("Seçilen Cihaz VendorId: " + _vendorId);
+    let device;
+
+    navigator.usb.requestDevice({ filters: [{ vendorId: _vendorId }] })
+        .then(selectedDevice => {
+            device = selectedDevice;
+            return device.open(); // Begin a session.
+        })
+
+        .then(() => device.selectConfiguration(1)) // Select configuration #1 for the device.
+        .then(() => device.claimInterface(2)) // Request exclusive control over interface #2.
+        .then(() => device.controlTransferOut({
+            requestType: 'class',
+            recipient: 'interface',
+            request: 0x22,
+            value: 0x01,
+            index: 0x02
+        })) // Ready to receive data
+        .then(() => device.transferIn(5, 1)) // Waiting for 64 bytes of data from endpoint #5.
+        .then(result => {
+            const decoder = new TextDecoder();
+            console.log('Received: ' + decoder.decode(result.data));
+            alert('Received: ' + decoder.decode(result.data));
+        })
+        .catch(error => { console.error(error); });
+
+}
+
 
 async function listele() {
     $("#devices").html("");
@@ -14,20 +55,25 @@ async function listele() {
     counter = 0;
     navigator.usb.getDevices().then(function (devices) {
         devices.forEach(device => {
-            console.log(device);
-            $("#devices").append("<tr><td>" + counter++ + " </td><td>  " + device.productName +
-                " </td><td> " + device.serialNumber + "</td><td> " + device.manufacturerName + "</td><td> " + device.productId + "</td></tr>");
+            $("#devices").append(""
+                + "<tr> <td class='p-1'>" + counter++
+                + "</td><td class='p-1'>  " + " <button class='btnSec btn btn-sm btn-info' onclick=SecileneIzinIste(" + device.vendorId + ")> Seç</button>"
+                + "</td><td class='p-1'>  " + device.productName
+                + "</td><td class='p-1'>  " + device.vendorId
+                + "</td><td class='p-1'> " + device.serialNumber
+                + "</td><td class='p-1'> " + device.manufacturerName
+                + "</td><td class='p-1'> " + device.productId + "</td></tr>");
         });
         $("#algilanan").html("Algılanan: " + counter);
 
     });
-   
+
 
 }
 
 
 navigator.usb.addEventListener('connect', event => {
-    alert("Cihaz Bağlandı");
+    izinIste()
 });
 
 navigator.usb.addEventListener('disconnect', event => {
